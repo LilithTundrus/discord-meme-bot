955453d783c5ab1eb66c4969f3d6f5fad0dd9042
Lilith Tundrus <echofox123@gmail.com>
2020-06-13 21:37:18 -0400

    Skeleton of project started

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Force ES6 strict mode on compiled JS
+'use strict';
+
+// Custom code imports
+
+// Node native imports
+
+// NPM package imports
+import * as Discord from 'discord.js';
+
+// Global declarations
+const client = new Discord.Client();
+const prefix = '!!';
+
