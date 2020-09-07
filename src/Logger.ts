9622fc1322163741bb200c39bac6cacb5e340721
Lilith Tundrus <echofox123@gmail.com>
2020-09-06 21:13:44 -0400

    I think this is done probably, fuck

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,9 +1,10 @@
 // Force ES6 strict mode on compiled JS
-"use strict";
+'use strict';
 
 // Custom code imports
 
 // Node native imports
+import * as fs from 'fs';
 
 // NPM package imports
 
@@ -13,20 +14,43 @@
 // using the big logging tools because they're a subject to learn on their own
 // This will just be for use here after I read a bit more on actually good logging
 export default class Logger {
-    public filePath: string;
-    public logFileName: string;
+    public logPath: string;
+    private writeToFile: boolean;
 
-    constructor() {}
-
-    info() {}
-
-    warn() {}
-
-    error() {}
-
-    private constructLogMessage() {
-        // This function adds things like timestamps/etc.
+    constructor(writeToFile?: string, logPath?: string) {
+        // Don't bother checking the path being fed to this. Yeah blah blah
+        // "Don't trust your inputs" but fuck you I'm not making enterprise software
+        if (writeToFile && logPath) {
+            this.logPath = logPath;
+            this.writeToFile = true;
+        }
     }
 
-    private writeToFile() {}
+    debug(logMessage: string) {
+        this.constructLogMessage(logMessage, 'DEBUG');
+    }
+
+    info(logMessage: string) {
+        this.constructLogMessage(logMessage, 'INFO');
+    }
+
+    warn(logMessage: string) {
+        this.constructLogMessage(logMessage, 'WARN');
+    }
+
+    error(logMessage: string) {
+        this.constructLogMessage(logMessage, 'ERROR');
+    }
+
+    // Create the log message based on given inputs
+    private constructLogMessage(initialMessage: string, logLevel: string) {
+        initialMessage = `[${Date.now()}] ${logLevel}: ${initialMessage}`;
+        if (this.writeToFile) {
+            this.writeToLogFile(initialMessage);
+        }
+    }
+
+    private writeToLogFile(logMessage: string) {
+        fs.appendFileSync(this.logPath, logMessage);
+    }
 }
