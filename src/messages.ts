fc19b6f85a0894a90e09158abe1b76012ca71090
Lilith Tundrus <echofox123@gmail.com>
2020-11-06 11:14:47 -0500

    Added more messages

--- src/messages.ts
+++ src/messages.ts
@@ -10,6 +10,8 @@
 // Global declarations
 
 // Variables used for storing message to keep things sort of modular
+// Only messages that do not need variables are stored here.
+// Short messages are also kept in the bot file
 const useAServer = 'Sorry, please use this bot in a valid Discord server. Thanks!';
 
 const successfulRegister =
@@ -21,4 +23,34 @@ const alreadyRegistered =
 const registrationError =
     'Sorry, something went wrong. Try again later. I will DM the bot admin and let them know something went wrong.';
 
-export { useAServer, successfulRegister, alreadyRegistered, registrationError };
+const successfulUnregister = 'This server has been unregistered with the bot!';
+
+const alreadyUnregistered = 'Sorry, it looks like this server is not registered.';
+
+const addArgsNeeded =
+    'Please give a subreddit to subscribe to with the `!!add` command\nExample: `!!add funny`';
+
+const addNeedToRegister = 'You need to initialize your server with the `register` command first.';
+
+const removeArgsNeeded =
+    'Please give a subreddit to unsubscribe to with the `!!remove` command\nExample: `!!remove funny`';
+
+const showSubsNoSubscriptions =
+    'It looks like this server had no subscribed subreddits. Add some with the `!!add` command!';
+
+const hotArgsNeeded =
+    'Please give a subreddit to switch to sorting by hot. Example: `!!sethot funny`';
+
+export {
+    useAServer,
+    successfulRegister,
+    alreadyRegistered,
+    registrationError,
+    successfulUnregister,
+    alreadyUnregistered,
+    addArgsNeeded,
+    addNeedToRegister,
+    removeArgsNeeded,
+    showSubsNoSubscriptions,
+    hotArgsNeeded,
+};
